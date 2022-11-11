c5e00871fd8fe8a040767f8fc494c89a95793a26
wfernandez4 <wfern024@fiu.edu>
2022-11-11 14:43:53 -0500

    Provide the basic skeleton of a LinkedList class and substituent Node class

---
+++ Data-Structures/LinkedList.java
@@ -0,0 +1,51 @@
+
+
+public class LinkedList {
+    Node head;
+    
+    // Empty constructor
+    LinkedList() {
+    }
+
+    LinkedList(Node head) {
+        this.head = head;
+
+
+    }
+
+    public class Node {
+        Node next;
+        int data;
+        
+        // Empty constructor
+        Node() {
+        }
+
+        Node(int data) {
+            this.data = data;
+        }
+        
+
+    }
+
+    // Append a node to the end of the list
+    public void appendNode(LinkedList list) {
+
+    }
+    
+    // Delete a node with the mathcing value
+    public void deleteNode (int data) {
+
+    }
+    // Print the values of all nodes in the LinkedList to the console
+    public void printLinkedList(){
+
+    }
+
+
+    public static void main(String[] args) {
+        
+    }
+
+
+}
